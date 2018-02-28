55f480d890ba2ffe559bdaa239215a7d2771965f
Frank H Mcmanus <frank@thefrankharvey.com>
2018-02-28 23:50:11 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,20 @@
 def my_select(collection)
- # your code here!
+if block_given?
+  i = 0
+  new_arr = []
+
+  while i < collection.length
+    yield collection[i]
+    if collection[i]
+    new_arr << collection[i]
+    i += 1
+  end
+  #iterate over elements
+  #if elements matches argument
+  #store element in new array
+  #return new array
+else
+  "No block given."
+end
+  new_arr
 end

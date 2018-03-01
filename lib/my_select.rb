cf6d06ca70922f6b08c40bb6b7916295a4169bc6
Frank H Mcmanus <frank@thefrankharvey.com>
2018-03-01 00:03:31 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,11 +3,11 @@ if block_given?
   i = 0
   new_arr = []
 
-  while i < collection.length
-    yield collection[i]
-    if collection[i]
-    new_arr << collection[i]
+  while i < collection.length 
+    new_arr.push(yield collection[i])
     i += 1
+  end
+  
 else
   "No block given."
 end
